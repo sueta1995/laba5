0ed94efd746df1a285a7b256488833be9ddd4541
sueta1995 <kitaev.tagir@gmail.com>
2022-10-12 22:16:16 +0300

    addedd tests for part 1 and 2

---
+++ task1_spec.rb
@@ -0,0 +1,9 @@
+require 'rspec'
+require_relative 'task1.rb'
+
+describe "task1" do
+  it 'should do something' do
+    expect(Prog.new(5, 4).calc).to eq -1.629970163625946
+    expect(Prog.new(-1, 6).calc).to eq -1.165093440025712
+  end
+end
---
+++ task2_spec.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+require_relative 'task2.rb'
+
+describe "task2" do
+  it 'should do something' do
+    expect(Prog.new("").check).to eq true
+
+    
+  end
+end
