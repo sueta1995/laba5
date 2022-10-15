ceb57af39a33d02b14eefdceefc4a6062db6836e
sueta1995 <tagir.kitaev@inbox.ru>
2022-10-15 13:05:13 +0300

    added test for part 2

--- task2_spec.rb
+++ task2_spec.rb
@@ -1,10 +1,30 @@
 require 'rspec'
 require_relative 'task2.rb'
 
-describe "task2" do
-  it 'should do something' do
-    expect(Prog.new("").check).to eq true
+require 'securerandom'
 
-    
+describe "task2" do
+  context 'when parameter is valid' do
+    it 'returns true if empty string' do
+      expect(Prog.new('').check).to eq true
+    end
+
+    it 'returns false if common string' do
+      expect(Prog.new(SecureRandom.alphanumeric).check).to eq false
+    end
+
+    it 'returns true if mirrored string' do
+      str = SecureRandom.alphanumeric
+      str += str.reverse
+
+      expect(Prog.new(str).check).to eq true
+    end
   end
+
+  context 'when parameter is nil' do
+    it 'raises error' do
+      expect { Prog.new(nil).check }.to raise_error
+    end
+  end
+
 end
